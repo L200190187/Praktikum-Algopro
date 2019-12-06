91df28aec1f32e98ef5cbc3bd92f1ad4e5b4080a
L200190187 <55913483+L200190187@users.noreply.github.com>
2019-12-06 07:45:39 +0700

    Add files via upload

---
+++ praktikum 10/kegiatan 1 server.py
@@ -0,0 +1,17 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", 50001))
+s.listen(5)
+print "Program Komunikasi Tentang Data Diri"
+data = ""
+kamus = {"nama":"Nurma Trisnawati", "NIM" : "L200190187", "angkatan" : "2019", "keluar" : "Siap!"}
+while data.lower() != "keluar":
+    komm, addr = s.accept()
+    while data.lower() != "keluar":
+        data = komm.recv(1024)
+        print "perintah :", data
+        if kamus.has_key(data):
+            komm.send(kamus[data])
+        else:
+            komm.send("Maaf, Perintah Tidak Dimengerti")
